301c809fbb53eef1da997ff353256e001ff408c8
SimoCat2000 <simonecatalani2000@gmail.com>
2025-07-08 09:21:36 +0200

    Update Esercizi_Riepilogo_JS.js

--- Esercizi_Riepilogo_JS.js
+++ Esercizi_Riepilogo_JS.js
@@ -196,6 +196,16 @@ console.log(checkUser(utente1))
 ////////////////////
 // ESERCIZIO 7
 // Scrivi una funzione 'asterischi()' che accetti come parametro un numero e che tramite un ciclo `for` costuisca una stringa composta un totale di asterischi uguale al numero passato come parametr. Invoca la funzione stampando il risulato in console. **Attenzione: non usare il console.log() all'interno della funzione! la funzione deve ritornare la stringa**
+function asterischi(num){
+  let risultato = ""
+  for(let i = 0; i < num; i++){
+     (risultato += "*" )
+
+  }
+  return risultato
+ 
+}
+console.log(asterischi(10))
 ////////////////////
 
 ////////////////////
